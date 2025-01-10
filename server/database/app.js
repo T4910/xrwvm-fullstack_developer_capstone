1e5c9b6bb330ccad81a34aef03dd8c5bb1c42f89
T4910 <ttaiwo4910@gmail.com>
2025-01-10 08:06:49 +0100

    link issues & register models to admin

--- server/database/app.js
+++ server/database/app.js
@@ -1,10 +1,10 @@
+/*jshint esversion: 8 */
 const express = require('express');
 const mongoose = require('mongoose');
 const fs = require('fs');
 const  cors = require('cors')
 const app = express()
 const port = 3030;
-
 app.use(cors())
 app.use(require('body-parser').urlencoded({ extended: false }));
 
